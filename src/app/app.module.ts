b41d40d00a73e8ae9773268067802eab725a8872
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2021-02-19 13:54:28 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalcComponent } from './calc/calc.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalcComponent,
+    ToolbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
