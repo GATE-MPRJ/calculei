5c005f1d7cd45bf5081cdd8591d3709aacd1e1d2
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-04-14 08:04:06 -0300

    New Layout

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
+
+
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+@Component({
+  selector: 'dialog-orientacoes',
+  templateUrl: 'dialog-orientacoes.html',
+  styleUrls: ['dialog-orientacoes.scss']
+})
+export class DialogContentExampleDialog {}
